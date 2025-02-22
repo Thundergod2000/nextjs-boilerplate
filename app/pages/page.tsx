93baf33b22829b42d7eb90aa27e62a4305dddda0
Thundergod2000 <93822554+Thundergod2000@users.noreply.github.com>
2025-02-22 12:08:31 +0530

    Bug Fix for Initial Commit

--- app/pages/page.tsx
+++ app/pages/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react"
 
 export default function Pages() {
-    const [inputforApiTest, setInputforApiTest] = useState("")
+    // const [inputforApiTest, setInputforApiTest] = useState("")
     return(
         <div>
             <input
