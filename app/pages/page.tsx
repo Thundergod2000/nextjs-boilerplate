58291d053ac5e7fa6af9d63fb8c3b2151ff9322f
Thundergod2000 <93822554+Thundergod2000@users.noreply.github.com>
2025-02-22 12:27:14 +0530

    Bug fix 2 for Initial Commit

--- app/pages/page.tsx
+++ app/pages/page.tsx
@@ -1,13 +1,22 @@
 "use client"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Pages() {
-    // const [inputforApiTest, setInputforApiTest] = useState("")
+    const [inputforApiTest, setInputforApiTest] = useState("")
+    // setInputforApiTest('Test deployment')
+    useEffect(()=>{
+        // setInputforApiTest("Deploy")
+    })
+    function setInputforApi(value: string){
+        setInputforApiTest(value)
+    }
     return(
         <div>
             <input
                 name="Test"
                 placeholder="Test"
+                value={inputforApiTest}
+                onChange={(e) => setInputforApi(e.target.value)}
                 className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             />
         </div>
